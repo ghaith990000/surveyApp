d01c763b3e34d6ab95957ee9dff244be6f7855a1
Ghaith <ghaith990000@gmail.com>
2023-11-04 18:03:35 +0300

    Add navigation for the dashboard and surveys

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,27 +1,45 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { Navigate, createBrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import Dashboard from './views/Dashboard';
 import Surveys from './views/Surveys';
 import Login from './views/Login';
 import Signup from './views/Signup';
+import GuestLayout from './components/GuestLayout';
+import DefaultLayout from './components/DefaultLayout';
 const router = createBrowserRouter([
   {
-    path: '/app',
-    element: <Dashboard />
+    path: "/",
+    element: <DefaultLayout />,
+    children: [
+      {
+        path: '/dashboard',
+        element: <Navigate to="/" />
+      },
+      {
+        path: "/",
+        element: <Dashboard />,
+      },
+      {
+        path: "/surveys",
+        element: <Surveys />
+      }
+    ]
   },
   {
-    path: '/surveys',
-    element: <Surveys />
-  },
-  {
-    path: '/login',
-    element: <Login />
-  },
-  {
-    path: '/signup',
-    element: <Signup />
-  },
+    path: '/',
+    element: <GuestLayout />,
+    children: [
+      {
+        path: 'login',
+        element: <Login />
+      },
+      {
+        path: 'signup',
+        element: <Signup />
+      },
+    ]
+  }
 ])
 
 export default router;
